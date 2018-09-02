8667b8bcc91977ec9bae67040e1984e47d55e117
Charles Zhang <charlesdev99@gmail.com>
2018-09-02 02:02:01 -0400

    4.16 player now moves

--- Assets/2D Galaxy Assets/Scripts/Player.cs
+++ Assets/2D Galaxy Assets/Scripts/Player.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public float speed = 5.0f;
+  
 	// Use this for initialization
 	void Start () {
-        //Debug.Log("Name: " + name);
+
         transform.position = new Vector3(0, 0, 0);
 	}
 
     // Update is called once per frame
     void Update(){
-        
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontalInput, 0, 0) * Time.deltaTime * speed);
+        transform.Translate(new Vector3(0, verticalInput, 0) * Time.deltaTime * speed);
     }
 }
