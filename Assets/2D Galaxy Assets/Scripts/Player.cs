d82cce8e8cc50e343138befa889ae3514c5b27a0
Charles Zhang <charlesdev99@gmail.com>
2018-09-02 02:57:41 -0400

    4.20 Cleaned up scenes

--- Assets/2D Galaxy Assets/Scripts/Player.cs
+++ Assets/2D Galaxy Assets/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public float speed = 5.0f;
+    [SerializeField] //This allows for the variable to be private but still able to view in Unity Inspector
+    private float speed = 5.0f;
   
 	// Use this for initialization
 	void Start () {
@@ -14,6 +15,12 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame
     void Update(){
+        Movement();
+
+    }
+
+
+    private void Movement(){
 
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
@@ -21,27 +28,22 @@ public class Player : MonoBehaviour {
         transform.Translate(new Vector3(horizontalInput, 0, 0) * Time.deltaTime * speed);
         transform.Translate(new Vector3(0, verticalInput, 0) * Time.deltaTime * speed);
 
-        if (transform.position.y > 0)
-        {
+        if (transform.position.y > 0){
             transform.position = new Vector3(transform.position.x, 0, 0);
         }
 
-        else if (transform.position.y < -4.2f)
-        {
+        else if (transform.position.y < -4.2f){
             transform.position = new Vector3(transform.position.x, -4.2f, 0);
         }
 
-        if (transform.position.x > 9.4)
-        {
+        if (transform.position.x > 9.4){
             transform.position = new Vector3(-9.4f, transform.position.y, 0);
         }
 
-        if (transform.position.x < -9.4)
-        {
+        if (transform.position.x < -9.4){
             transform.position = new Vector3(9.4f, transform.position.y, 0);
         }
-    
-    
     }
+
 
 }
