05b7209e8d0904218f9edc1c5e833db328f717ad
Charles Zhang <charlesdev99@gmail.com>
2018-09-08 18:52:36 -0400

    deleted test.txt

---
+++ Assets/2D Galaxy Assets/Scripts/Powerup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour {
+
+    [SerializeField]
+    private float _speed = 3;
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Translate(Vector3.down * Time.deltaTime * _speed);
+	}
+}
